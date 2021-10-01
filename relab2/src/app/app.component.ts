7ca89a4aaaa3516a05b45f785939a18acac01300
Siriani1 <siriani.andrea@iisgalvanimi.edu.it>
2021-10-01 09:47:09 +0000

    aggiunto icon e triangolo

--- relab2/src/app/app.component.ts
+++ relab2/src/app/app.component.ts
@@ -17,7 +17,19 @@ export class AppComponent {
   center_punto: { lat: number; lng: number; };
   position_punto: { lat: number; lng: number; };
   circleOption_punto: { fillColor: string; };
+  center_punto2: { lat: number; lng: number; };
+  position_punto2: { lat: number; lng: number; };
+  circleOption_punto2: { fillColor: string; };
+  center_punto3: { lat: number; lng: number; };
+  position_punto3: { lat: number; lng: number; };
+  circleOption_punto3: { fillColor: string; };
 
+  markerOptions: google.maps.MarkerOptions;
+  markerOption: google.maps.MarkerOptions;
+  markerOptions2: google.maps.MarkerOptions;
+  markerOptions3: google.maps.MarkerOptions;
+
+  vertices: google.maps.LatLngLiteral[];
   
   constructor()
   {
@@ -26,9 +38,49 @@ export class AppComponent {
     this.label = "Tu";
     this.circleOption = {fillColor : 'red'}
 
-    this.center_punto={lat:45.8754875,lng:9.73647363}
+    this.center_punto={lat:45.5097718,lng:9.189956}
     this.position_punto = this.center_punto
     this.circleOption_punto = {fillColor : 'blue'}
+
+    this.center_punto2 = {lat:45.5084981,lng:9.1915138}
+    this.position_punto2 = this.center_punto2
+    this.circleOption_punto2 = {fillColor : 'purple'}
+
+    this.center_punto3 = {lat:45.5076113,lng:9.1933876}
+    this.position_punto3 = this.center_punto3
+    this.circleOption_punto3 = {fillColor : 'yellow'}
+
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+    
+    let icon : google.maps.Icon = {
+      url:'./assets/img/cow.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+
+    let iconData2 : google.maps.Icon = {
+      url:'./assets/img/owl.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+
+    let iconData3 : google.maps.Icon = {
+      url:'./assets/img/turtle.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+
+    this.markerOptions = {icon:iconData}
+    this.markerOption = {icon:icon}
+    this.markerOptions2 = {icon:iconData2}
+    this.markerOptions3 = {icon:iconData3}
+
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+    ];
+    
   }
 
   colore_giallo(){
